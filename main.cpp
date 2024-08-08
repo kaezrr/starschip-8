ceee3da6513517d8ffe9a034b3e995c6d6dfb455
kaezrr <107052359+kaezrr@users.noreply.github.com>
2024-08-09 00:17:21 +0530

    fixed cpu speed comment

--- main.cpp
+++ main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char* argv[]) {
 
         int ipf = (chip8.super) ? 30 : 12;
 
-        // CPU runs at 720Hz or 1.8MHz w/ SCHIP
+        // CPU runs at 720Hz or 1.8KHz w/ SCHIP
         for (int _ = 0; _ < ipf; ++_) {
             chip8.fetch();
             chip8.decode_and_execute();
